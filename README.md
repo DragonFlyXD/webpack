f8b7773331eee59dfbbd243b1956208a788f1e18
DragonFly <dragonfly920130@outlook.com>
2018-01-29 11:40:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## Intro
+
+自定义基于**Webpack**的Vue.js项目模板。
+
+## How To Use
+
+执行以下命令即可：
+
+```
+vue init DragonFlyXD/webpack myproject
+```
+
---
+++ template/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <p>HELLO</p>
+    {{#router}}
+    <router-view/>
+    {{/router}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
